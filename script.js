67d6d4f7d88fe2dcd9a0f8aa3fe71d711a292c47
omni <mohamedelshazly18@yahoo.com>
2022-10-13 18:31:26 +0200

    done with game loop logic, wins and draws, and reseting game

--- script.js
+++ script.js
@@ -40,8 +40,15 @@ const GameBoard = (function gameBoardModule() {
 
 //players factory 
 const playersFactory = (marker, board) => {
+    let containers = {
+        rowContainer: [0, 0, 0],
+        columnContainer: [0, 0, 0],
+        diagContainer: [0, 0, 0],
+        oppositeDiagContainer: [0, 0, 0]
+    }
     return {
         marker,
+        containers,
         move: (pos) => {
             return board.isLegal(pos) ? pos : null;
         }
@@ -49,6 +56,78 @@ const playersFactory = (marker, board) => {
 };
 
 const GameLoop = (function GameLoopModule() {
+    const _checkWin = (row, col, player) => {
+
+        /* 
+            credit for the algorithm goes to this guy! 
+            https://jayeshkawli.ghost.io/tic-tac-toe/
+        */
+
+        //process for figuring out wins along rows and columns
+        player.containers.rowContainer[row] += 1;
+        player.containers.columnContainer[col] += 1;
+
+        if (row == col) {
+            player.containers.diagContainer[row] += 1;
+        };
+
+        if ((row + col + 1) == 3) {
+            player.containers.oppositeDiagContainer[row] += 1;
+        };
+
+        let diagonalElementsSum = 0;
+        let oppositeDiagonalElementsSum = 0;
+
+        for (let i = 0; i < 3; i++) {
+            diagonalElementsSum += player.containers.diagContainer[i];
+            oppositeDiagonalElementsSum += player.containers.oppositeDiagContainer[i];
+        };
+
+        //checks for wins along columns or rows
+        if (player.containers.rowContainer[row] == 3 || player.containers.columnContainer[col] == 3) {
+            return player.marker;
+        }
+        //check for wins along diagonals 
+        if (diagonalElementsSum == 3 || oppositeDiagonalElementsSum == 3) {
+            return player.marker;
+        }
+    };
+
+    const _checkDraw = () => {
+        let full = 0;
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            if (square.innerText) {
+                full += 1;
+            }
+        });
+        return full < 9 ? false : true;
+    }
+
+    const _resetGame = (players) => {
+
+        document.querySelector('.winner').innerText = '';
+
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.innerText = ''
+        });
+
+        players.p1.containers = {
+            rowContainer: [0, 0, 0],
+            columnContainer: [0, 0, 0],
+            diagContainer: [0, 0, 0],
+            oppositeDiagContainer: [0, 0, 0]
+        };
+
+        players.p2.containers = {
+            rowContainer: [0, 0, 0],
+            columnContainer: [0, 0, 0],
+            diagContainer: [0, 0, 0],
+            oppositeDiagContainer: [0, 0, 0]
+        };
+    };
+
     const initGame = () => {
         GameBoard.initSetup();
     };
@@ -58,30 +137,65 @@ const GameLoop = (function GameLoopModule() {
         const p2 = playersFactory('O', GameBoard);
 
         let current_player = p1;
-
+        let pos;
+        let row;
+        let col;
+        let winner;
         const squares = document.querySelectorAll('.square');
         squares.forEach((square) => {
             square.addEventListener('click', () => {
-                let pos = square.id;
-                pos = current_player.move(pos);
-                if(pos) {
-                    document.getElementById(pos).innerText = current_player.marker;
-                    current_player = current_player === p1 ? p2 : p1;
+                //to prevent clicks after game has been won
+                if (winner) {
+                    return;
                 }
+                else {
+                    pos = square.id;
+                    pos = current_player.move(pos);
+                    //put the marker down and switch players
+                    if (pos) {
+                        document.getElementById(pos).innerText = current_player.marker;
 
+                        //get row and col clicked
+                        row = Number(pos[0]);
+                        col = Number(pos[1]);
+
+                        winner = _checkWin(row, col, current_player);
+                        if (winner) {
+                            document.querySelector('.winner').innerText = `Winner is ${winner}`
+                        }
+                        if (_checkDraw()) {
+                            document.querySelector('.winner').innerText = "It's a draw!"
+                        }
+
+                        current_player = current_player === p1 ? p2 : p1;
+                    }
+
+                }
             })
-        });
 
-    }
+        });
+        document.querySelector('.reset').addEventListener('click', () => {
+            current_player = p1;
+            let players = {
+                p1,
+                p2
+            }
+            winner = undefined;
+            _resetGame(players);
+        })
+
+    };
 
     return {
         initGame,
-        startGame
+        startGame,
     }
 })();
 
 //driver code! 
 GameLoop.initGame();
-
 GameLoop.startGame();
+
+
+
 
